2d20fae15345ccf42c8b7d0a4716d6c0d3735d9a
Leon3854 <frad109@mail.ru>
2021-08-08 00:59:05 +0500

    Finish wort fot the plugin filter block

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('Hi! Bro!!!');
+const filterBox = document.querySelectorAll('.box');
+
+document.querySelector('nav').addEventListener('click', (event)=> {
+  if (event.target.tagName !== 'LI') return false;
+
+  const filterClass = event.target.dataset['f'];
+  // console.log(filterClass);
+
+  filterBox.forEach((elem) => { //Через цикол перебераем весь список
+    elem.classList.remove('hide');
+    if (!elem.classList.contains(filterClass) && filterClass !== 'all') { // Здесь условие такое - Если нет в выбранного нужного элемента дома дерева то тогда 
+      //elem.classList - это связка сам classList является геттером (https://developer.mozilla.org/ru/docs/Web/API/Element/classList)
+      elem.classList.add('hide')
+    }
+  })
+})
